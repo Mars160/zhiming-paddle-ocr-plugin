01eed5881f631f9a4adbe0a3525926197b1134a4
Mars160 <wapollo9@gmail.com>
2023-04-09 13:06:41 +0800

    修改ocr插件工作模式

--- backend/resource.py
+++ backend/resource.py
@@ -1,57 +1,19 @@
-import logging
-import os
-from threading import Thread
-
 from flask_restful import Resource
 from flask import request
 from flask_jwt_extended import jwt_required
-from plugins.tunnel import response_base
-import uuid
-
-from .ext import *
-from .ocr import *
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['jpg', 'jpeg', 'png']
-
+from requests import get, post
 
 class OCR(Resource):
-    OCR_DICT = {}
+    OCR_URL = 'http://localhost:65533/ocr'
 
-    # @jwt_required()
+    @jwt_required()
     def get(self, uuid):
-        response = response_base.copy()
-        if uuid in self.OCR_DICT:
-            if self.OCR_DICT[uuid] != 0:
-                response['data'] = self.OCR_DICT[uuid].copy()
-                del self.OCR_DICT[uuid]
-            else:
-                response['data'] = None
-                response['msg'] = '正在识别中...'
-        else:
-            response['backend'] = 404
-            response['msg'] = 'OCR任务不存在'
-        logging.warning(self.OCR_DICT)
-        return response
+        req = get('{}/{}'.format(self.OCR_URL, uuid))
+        return req.json()
 
-    # @jwt_required()
+    @jwt_required()
     def post(self):
-        response = response_base.copy()
         file = request.files['file']
-        if file and allowed_file(file.filename):
-            random_name = '{}'.format(uuid.uuid4().hex)
-            file.save('{}{}{}'.format(TEMP_DIR, os.sep, random_name))
+        req = post(self.OCR_URL, files={'file': file})
+        return req.json()
 
-            # 新开一个线程来识别
-            p = Thread(target=self.start_ocr, args=(random_name,))
-            p.start()
-            response['data'] = random_name
-        return response
-
-    def start_ocr(self, filename):
-        filepath = '{}{}{}'.format(TEMP_DIR, os.sep, filename)
-        self.OCR_DICT[filename] = 0
-        result = ocr(filepath)
-        self.OCR_DICT[filename] = result
-        os.remove(filepath)
