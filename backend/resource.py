b1fa4a858f4e9b982afad23a7ce9c49aad306fde
Mars160 <wapollo9@gmail.com>
2023-04-18 13:59:58 +0800

    未安装提示

--- backend/resource.py
+++ backend/resource.py
@@ -3,17 +3,31 @@ from flask import request
 from flask_jwt_extended import jwt_required
 from requests import get, post
 
+
 class OCR(Resource):
     OCR_URL = 'http://localhost:65533/ocr'
 
     @jwt_required()
     def get(self, uuid):
-        req = get('{}/{}'.format(self.OCR_URL, uuid))
-        return req.json()
+        try:
+            req = get('{}/{}'.format(self.OCR_URL, uuid))
+            return req.json()
+        except:
+            return {
+                "code": 500,
+                "data": None,
+                "msg": "OCR后端未部署"
+            }
 
     @jwt_required()
     def post(self):
-        file = request.files['file']
-        req = post(self.OCR_URL, files={'file': file})
-        return req.json()
-
+        try:
+            file = request.files['file']
+            req = post(self.OCR_URL, files={'file': file})
+            return req.json()
+        except:
+            return {
+                "code": 500,
+                "data": None,
+                "msg": "OCR后端未部署"
+            }
